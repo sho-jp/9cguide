cc40bf9f2ac3779bd4d674ab65a0c8e7d815654a
sho171 <sh.zxcv00@gmail.com>
2022-03-06 15:08:35 +0900

    add

---
+++ 3/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello\tWorld!\n");
+	return 0;
+}
---
+++ 4/integer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("result: %d\n", (1 + 100) * 100 / 2);
+    printf("result: %f\n", 10.0 + 3.0);
+    printf("div: %d mod: %d\n", 40 / 13, 40 % 13);
+    return 0;
+}
